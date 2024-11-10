6b053ef79be57c2ad7423e3669d756fa1ff3b62e
Konstantinos Lampridis <boromir674@hotmail.com>
2024-11-11 00:00:48 +0200

    feat(tf): define infrastructure as Code for API Gateway and integration with URL Provider Lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,3 +65,85 @@ module "presigned_url_lambda" {
 #   api_name    = var.api_name                           # API name per environment
 #   lambda_arns = [module.budget_check_lambda.lambda_arn]  # Links the budget check Lambda
 # }
+
+### Create an AWS API Gateway to expose the URL generation Lambda ###
+# Resource: API Gateway to expose the URL generation Lambda
+resource "aws_api_gateway_rest_api" "url_provider_api" {
+  name        = "URLProviderAPI"
+  description = "API to generate pre-signed URLs for S3 uploads"
+}
+
+# Define an HTTP endpoint (aka resource path, url path) in the API Gateway
+resource "aws_api_gateway_resource" "url_resource" {
+  rest_api_id = aws_api_gateway_rest_api.url_provider_api.id
+  parent_id   = aws_api_gateway_rest_api.url_provider_api.root_resource_id
+  path_part   = "get-presigned-url"
+}
+
+# # Define a POST method for API Gateway
+# resource "aws_api_gateway_method" "post_presigned_url" {
+#   rest_api_id   = aws_api_gateway_rest_api.url_provider_api.id
+#   resource_id   = aws_api_gateway_resource.url_resource.id
+#   http_method   = "POST"                     # Change to POST
+#   authorization = "NONE"                      # No auth for quick testing
+
+#   # Specify that the request will have a JSON payload
+#   request_parameters = {
+#     "method.request.header.Content-Type" = true
+#   }
+# }
+
+# Enable the GET method for above Endpoint
+resource "aws_api_gateway_method" "get_presigned_url" {
+  rest_api_id   = aws_api_gateway_rest_api.url_provider_api.id
+  resource_id   = aws_api_gateway_resource.url_resource.id
+  http_method   = "GET"
+  authorization = "NONE"  # No auth for quick testing; replace with proper auth later
+}
+
+# # Integrate API Gateway with Lambda function for POST method
+# resource "aws_api_gateway_integration" "lambda_integration" {
+#   rest_api_id             = aws_api_gateway_rest_api.url_provider_api.id
+#   resource_id             = aws_api_gateway_resource.url_resource.id
+#   http_method             = aws_api_gateway_method.post_presigned_url.http_method
+#   integration_http_method = "POST"             # POST to match Lambda’s POST handler
+#   type                    = "AWS_PROXY"        # AWS_PROXY to handle Lambda responses directly
+#   uri                     = aws_lambda_function.presigned_url_lambda.invoke_arn
+
+#   request_templates = {
+#     "application/json" = <<EOF
+#       #if($input.json('$') != '')
+#         $input.json('$')
+#       #else
+#         {}
+#       #end
+#     EOF
+#   }
+# }
+
+# Integrate API Gateway with the Lambda function
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.url_provider_api.id
+  resource_id             = aws_api_gateway_resource.url_resource.id
+  http_method             = aws_api_gateway_method.get_presigned_url.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.presigned_url_lambda.lambda_invoke_arn
+}
+
+# Grant API Gateway permission to invoke the URL Provider Lambda
+resource "aws_lambda_permission" "api_gateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = module.presigned_url_lambda.lambda_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.url_provider_api.execution_arn}/*/*"
+}
+
+# Deploy the API
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on   = [aws_api_gateway_integration.lambda_integration]
+  rest_api_id  = aws_api_gateway_rest_api.url_provider_api.id
+  # nest all resources/endpoints under /test path
+  stage_name   = "test"
+}
--- terraform/modules/aws_lambda/outputs.tf
+++ terraform/modules/aws_lambda/outputs.tf
@@ -9,3 +9,8 @@ output "lambda_name" {
   description = "Name of the Lambda function"
   value       = aws_lambda_function.generate_presigned_url.function_name
 }
+
+output "lambda_invoke_arn" {
+  description = "ARN to invoke the Lambda function"
+  value       = aws_lambda_function.generate_presigned_url.invoke_arn
+}
